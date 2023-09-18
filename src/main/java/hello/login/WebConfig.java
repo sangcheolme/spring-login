6bc6dd5e31eea4cc86f811d25a1dcb92d0edd08d
SangCheol <scpark0698@gmail.com>
2023-09-18 16:09:02 +0900

    bug fix 회원가입

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(2)
                 .addPathPatterns("/**")
                 .excludePathPatterns(
-                        "/", "members/add", "/login", "/logout",
+                        "/", "/members/add", "/login", "/logout",
                         "/css/**", "/*.ico", "/error"
                 );
     }
